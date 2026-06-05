1bc967accc7e6343c4d6821cfa2c559c6cc77592
brinda410 <brindarswamy@gmail.com>
2026-06-05 13:15:04 +0530

    Class and Objects1

---
+++ classObjects2/classObj2.java
@@ -0,0 +1,67 @@
+package classObjects2;
+
+public class classObj2 {
+    public static void main(String[]args){
+        //Employee1
+        Employee employee1=new Employee();
+        employee1.name="Brinda";
+        employee1.age=21;
+        employee1.salary=2100000;
+        System.out.println("Employee 1");
+        System.out.println(employee1.name);
+        System.out.println(employee1.age);
+        System.out.println(employee1.salary);
+
+        //Employee2
+        Employee employee2=new Employee();
+        employee2.name="Rahul";
+        employee2.age=21;
+        employee2.salary=2500000;
+        System.out.println("Employee 2");
+        System.out.println(employee2.name);
+        System.out.println(employee2.age);
+        System.out.println(employee2.salary);
+
+        //Employee 3
+        Employee employee3=new Employee();
+        employee3.name="Rohan";
+        employee3.age=21;
+        employee3.salary=1800000;
+        System.out.println("Employee 3");
+        System.out.println(employee3.name);
+        System.out.println(employee3.age);
+        System.out.println(employee3.salary);
+
+        //Employee4
+        Employee employee4=new Employee();
+        employee4.name="Kriti";
+        employee4.age=21;
+        employee4.salary=2800000;
+        System.out.println("Employee 4");
+        System.out.println(employee4.name);
+        System.out.println(employee4.age);
+        System.out.println(employee4.salary);
+
+        //Employee5
+        Employee employee5=new Employee();
+        employee5.name="Kiara";
+        employee5.age=21;
+        employee5.salary=2100000;
+        System.out.println("Employee 5");
+        System.out.println(employee5.name);
+        System.out.println(employee5.age);
+        System.out.println(employee5.salary);
+
+
+
+    }
+}
+class Employee{
+    String name;
+    int age;
+    int salary;
+
+
+}
+    
+
